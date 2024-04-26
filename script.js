db8563d86fedce9315fa2aee8beb09e658af5cc0
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2024-04-26 08:56:44 +0300

    Add function to switch to next image

--- script.js
+++ script.js
@@ -16,3 +16,37 @@ dropdownParent.forEach(dropdown => {
     });
 
 })
+//////
+
+// each image has an index. there is a currentIndex that changes depending according to arrows/buttons
+let currentIndex = 1
+// const child = document.querySelector(`.img-container:nth-child(${currentIndex})`);
+
+function displayImage(currentIndex) {
+    const currentImage = document.querySelector(`.img-container>img:nth-child(${currentIndex})`);
+    currentImage.style.display = "inline";
+    return currentIndex;
+}
+
+const test = document.createElement("div");
+test.innerHTML = "test";
+let container = document.getElementById("test-container")
+container.appendChild(test)
+
+test.addEventListener("click", shownNextImage)
+function shownNextImage(){
+    let previousImage = document.querySelector(`.img-container>img:nth-child(${currentIndex})`)
+    previousImage.style.display = "none";
+    currentIndex += 1;
+    console.log(currentIndex)
+    if (currentIndex > 4) {
+        currentIndex = 1;
+    }
+    else if (currentIndex < 1) {
+        currentIndex = 4;
+    }
+    console.log(currentIndex)
+
+    displayImage(currentIndex)
+    return currentIndex
+}
